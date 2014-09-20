608ca3d9cb24783022eb7be18f7082b1c140600d
CJ Wang <cjwang18@gmail.com>
2014-09-20 00:57:25 -0700

    Add all slideshow photos to photography page and enhance isotope

--- js/photography.js
+++ js/photography.js
@@ -2,7 +2,36 @@ var photography  = {
     "categories": [
         {
             "name": "nature",
-            "photos": []
+            "photos": [
+                {
+                    "fileNum": "6939",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "7484",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "9055",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "9113",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "9157",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "9292",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "9522",
+                    "fromSlideshow": true
+                },
+            ]
         },
         {
             "name": "people",
@@ -27,11 +56,77 @@ var photography  = {
         },
         {
             "name": "places",
-            "photos": []
+            "photos": [
+                {
+                    "fileNum": "0156",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "0684",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "1362",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "1373",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "1417",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "1442",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "7934",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "8977",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "9220",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "9330",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "9400",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "9886",
+                    "fromSlideshow": true
+                },
+            ]
         },
         {
             "name": "things",
-            "photos": []
+            "photos": [
+                {
+                    "fileNum": "6761",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "6800",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "8989",
+                    "fromSlideshow": true
+                },
+                {
+                    "fileNum": "9464",
+                    "fromSlideshow": true
+                },
+            ]
         }
     ]
 };
@@ -46,16 +141,23 @@ $(function() {
 });
 
 // Use isotope to render photography content
-$(document).ready(function () {
+// $(document).ready(function () {
+$(function() {
     var $container = $('#container').imagesLoaded(function() {
         $container.isotope({
             // options
             itemSelector: '.item',
             sortBy: 'random',
+            animationEngine: 'best-available',
             masonry: {
                 columnWidth: '.item',
                 gutter: 0
             }
         });
+    });
+    // filter items
+    $('a.filter').click(function() {
+        var filterValue = $(this).attr('data-filter');
+        $container.isotope({ filter: filterValue });
     });
 });
