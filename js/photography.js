c9739baab3e2ed95d259a8cd053e805a0df46808
CJ Wang <cjwang18@gmail.com>
2014-09-23 22:43:56 -0700

    Implement infinite scrolling and change links to photography page

--- js/photography.js
+++ js/photography.js
@@ -131,23 +131,28 @@ var photography  = {
     ]
 };
 
-// Use Handlebars templating to fill in photography HTML content
 $(function() {
+    // Use Handlebars templating to fill in photography HTML content
     var source   = $("#photo-template").html(),
         template = Handlebars.compile(source);
     var html = template(photography);
+    var itemArr = shuffleArray($.map($.parseHTML(html), function(val, i) {
+        if ($(val).hasClass('item'))
+            return val;
+    }));
 
-    $("#container").append(html);
-});
+    var i = 0;
+    var items = getItemsHTML(i, determineNumItems(), itemArr, function(count) {
+        i += count;
+    });
+    $('#container').append(items);
 
-// Use isotope to render photography content
-$(function() {
+    // Use isotope to render photography content
     var $container = $('#container').imagesLoaded(function() {
         // initialize isotope
         $container.isotope({
             // options
             itemSelector: '.item',
-            sortBy: 'random',
             animationEngine: 'best-available',
             masonry: {
                 columnWidth: '.item'
@@ -167,4 +172,88 @@ $(function() {
             $container.isotope('shuffle');
         }
     });
+
+    // custom infinite scrolling implementation
+    $(window).scroll(function() {
+        // only proceed if there are more images to insert
+        if (i < itemArr.length) {
+            if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+                $('body').append('<div id="tempLoad"></div>');
+                // insert the HTML of new images to temporary div
+                items = getItemsHTML(i, determineNumItems(), itemArr, function(count) {
+                    i += count;
+                });
+                $('#tempLoad').append(items);
+                // reduce opacity for animation purposes
+                $('#tempLoad').children().css({
+                    opacity: 0
+                });
+                // add images to isotope and animate in once they have loaded
+                var temp = $('#tempLoad').html();
+                $('#tempLoad').imagesLoaded(function() {
+                    $container.isotope('insert', $(temp), function() {
+                        $container.children().css({ opacity: 1 });
+                    });
+                    $container.isotope('layout');
+                });
+                // cleanup the temporary div
+                $('#tempLoad').remove();
+            }
+        }
+    });
 });
+
+/**
+ * Essentially acting as a hash table,
+ * returns the number of elements (isotope items)
+ * that should be added to the page based
+ * on the current window width.
+ */
+function determineNumItems() {
+    $('body').css('overflow', 'hidden');
+    var w = $(window).width();
+    $('body').css('overflow', 'auto');
+    if (w < 568) {
+        return 3;
+    } else if (w >= 568 && w < 1024) {
+        return 6;
+    } else if (w >= 1024 && w < 1280) {
+        return 9;
+    } else {
+        return 16;
+    }
+}
+
+/**
+ * Returns concatenated HTML string of
+ * n items from array arr starting at
+ * position i.
+ */
+function getItemsHTML(i, n, arr, callback) {
+    var l = i + n;
+    var html = "";
+    while ((i < l) && (i < arr.length)) {
+        // console.log('i: ' + i);
+        // console.log('l: ' + l);
+        // console.log('arr.length: ' + arr.length);
+        html += arr[i].outerHTML;
+        i++;
+    }
+    callback((n - (l - i)));
+    return html;
+}
+
+/**
+ * Randomize array element order in-place.
+ * Using Fisher-Yates shuffle algorithm.
+ * http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
